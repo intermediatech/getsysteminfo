83deb41f2a03e850dad262b669f737f367fd7bc8
Vishnu pradeep <intermedia.vishnu@gmail.com>
2022-09-27 15:58:13 +0530

    Added SMB Path

--- get.sh
+++ get.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+smbAddr=192.168.94.16
+
 mkdir asset_details
 cd asset_details
 lshw > lshw.log
@@ -7,3 +9,7 @@ lshw -html > lshw.html
 lscpu > cpu.log
 lsblk > block.log
 lspci > pci.log
+cd ..
+zip NV-PC-$1.asset_details.zip asset_details -r
+rm asset_details -Rf
+echo "" | smbclient //$smbAddr/systeminfo/ -c "put NV-PC-$1.asset_details.zip" > /dev/null
