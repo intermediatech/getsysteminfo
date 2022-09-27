e0e3b6c44797bffa7f048fbf7c03beb16d076134
Vishnu pradeep <intermedia.vishnu@gmail.com>
2022-09-27 16:04:05 +0530

    Remove the zip file

--- get.sh
+++ get.sh
@@ -13,3 +13,4 @@ cd ..
 zip NV-PC-$1.asset_details.zip asset_details -r
 rm asset_details -Rf
 echo "" | smbclient //$smbAddr/systeminfo/ -c "put NV-PC-$1.asset_details.zip" > /dev/null
+rm NV-PC-$1.asset_details.zip
